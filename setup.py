ba129db0bfd60a90efc4acc25ef9701de5e1300e
Mikko Kujala <m.kujala@live.com>
2020-06-01 13:52:20 +0300

    Added setup.py.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='blackjack',
+    version='0.1',
+    packages=['api', 'game'],
+    url='https://github.com/mkuja/jokujuttu',
+    license='BSD',
+    author='Mikko Kujala',
+    author_email='m.kujala@live.com',
+    description='Blackjack game and HTTP API for it.'
+)
